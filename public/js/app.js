2a0c8660ce8dfc372967382b111ef56119e24cde
Chris Mangum <mtdbass5@gmail.com>
2013-12-04 16:08:52 -0500

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+  $routeProvider.when('/r/:subreddit', {
+    templateUrl: 'layout.html',
+    controller: 'rbCtrl'
+  });
+}]);
+
+app.controller('rbCtrl', ['$scope', '$httpBackend', '$routeParams', 'jQuery', function ($scope, $httpBackend, $routeParams, $) {
+  $scope.data = '';
+
+  function parse(data) {
+    var i, max;
+    data = angular.fromJson(data).data.children;
+    for (i = 0, max = data.length; i < max; i += 1) {
+      data[i] = data[i].data;
+    }
+    return data;
+  }
+
+  function getData(params) {
+    var url = 'http://www.reddit.com/r/';
+    url += params.subreddit;
+    url += params.thread ? '/' + params.thread : '';
+    url += params.sort ? '/' + params.sort : '';
+
+    delete params.subreddit;
+    delete params.thread
+
+    params.limit = 1000;
+    url += '.json?';
+    url += $.param(params);
+    $httpBackend('GET', url, null, function (status, data) {
+      $scope.data = parse(data);
+      $scope.$apply();
+    });
+  }
+  getData($routeParams);
+}]);
+
+app.factory('jQuery', function() {
+  return jQuery;
+});
