d9df61ba6470433751839608db3bc4c36cfe9783
Chris Mangum <mtdbass5@gmail.com>
2013-12-06 14:34:52 -0500

    added comments page

--- public/js/app.js
+++ public/js/app.js
@@ -3,43 +3,52 @@ var app = angular.module('app', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/r/:subreddit', {
     templateUrl: 'layouts/posts.html',
-    controller: 'rbCtrl'
+    controller: 'rdtCtrl'
+  });
+  $routeProvider.when('/r/:subreddit/comments/:thread/:title', {
+    templateUrl: 'layouts/posts.html',
+    controller: 'rdtCtrl'
   });
 }]);
 
-app.controller('rbCtrl', [
-  '$scope',
-  '$httpBackend',
-  '$routeParams',
-  'jQuery',
-  function ($scope, $httpBackend, $routeParams, $) {
-    $scope.data = '';
+app.controller('rdtCtrl', ['$scope', 'http', '$routeParams', function ($scope, http, $routeParams) {
+    $scope.posts = [];
+    $scope.comments = [];
 
-    function parseData(data) {
+    function stripLayers(unstripped) {
+      var i, max, stripped = [];
+      unstripped = unstripped.data.children;
+      for (i = 0, max = unstripped.length; i < max; i += 1) {
+        stripped[i] = unstripped[i].data;
+        if (typeof stripped[i].replies === 'object') {
+          stripped[i].replies = stripLayers(stripped[i].replies);
+        }
+      }
+      return stripped;
+    }
+
+    function parseCData(data) {
+      var i, max;
+      data = angular.fromJson(data);
+      $scope.posts = [data[0].data.children[0].data];
+      $scope.comments = stripLayers(data[1]);
+    }
+
+    function parsePData(data) {
       var i, max;
       data = angular.fromJson(data).data.children;
       for (i = 0, max = data.length; i < max; i += 1) {
         data[i] = data[i].data;
       }
-      return data;
+      $scope.posts = data;
     }
 
-    function buildUrl(params) {
-      var url = 'http://www.reddit.com/r/';
-      url += params.subreddit;
-      url += params.thread ? '/comments/' + params.thread : '';
-      url += params.sort ? '/' + params.sort : '';
-
-      delete params.subreddit;
-      delete params.thread
-
-      params.limit = 1000;
-      url += '.json?';
-      return url + $.param(params);
-    }
-
-    $httpBackend('GET', buildUrl($routeParams), null, function (status, data) {
-      $scope.data = parseData(data);
+    http.get(function (data) {
+      if ($routeParams.thread) {
+        parseCData(data);
+      } else {
+        parsePData(data);
+      }
       $scope.$apply();
     });
   }
@@ -49,6 +58,30 @@ app.filter('timeago', ['moment', function (moment) {
   return function (timestamp) {
     return moment(timestamp * 1000).fromNow();
   };
+}]);
+
+app.factory('http', ['$httpBackend', '$routeParams', 'jQuery', function ($httpBackend, $routeParams, $) {
+  function buildUrl() {
+    var url = 'http://www.reddit.com/r/',
+      params = $.extend({}, $routeParams);
+    url += params.subreddit;
+    url += params.thread ? '/comments/' + params.thread : '';
+    url += params.sort ? '/' + params.sort : '';
+
+    delete params.subreddit;
+    delete params.thread
+
+    params.limit = 1000;
+    url += '.json?';
+    return url + $.param(params);
+  }
+  return {
+    get: function (callback) {
+      $httpBackend('GET', buildUrl(), null, function (status, data) {
+        callback(data);
+      });
+    }
+  }
 }]);
 
 app.factory('moment', function () {
