d92b9b6bb207fa8679cfd1808532ad818b2ea695
Chris Mangum <mtdbass5@gmail.com>
2014-01-14 14:53:43 -0500

    added the dev logger back

--- server/app.js
+++ server/app.js
@@ -1,11 +1,8 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var app = express();
+var express = require('express'),
+  http = require('http'),
+  path = require('path'),
+  app = express();
 
-app.set('port', 3000);
+app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(3000);
