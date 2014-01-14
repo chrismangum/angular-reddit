ffdafc50a7b1f9b3f057806ddd4f112446dbee49
Chris Mangum <mtdbass5@gmail.com>
2014-01-14 15:06:44 -0500

    limited server to localhost

--- server/app.js
+++ server/app.js
@@ -4,5 +4,13 @@ var express = require('express'),
   app = express();
 
 app.use(express.logger('dev'));
+app.use(function (req, res, next) {
+  if (req.connection.remoteAddress === '127.0.0.1') {
+    next();
+  } else {
+    console.log('invalid ip');
+    res.end();
+  }
+});
 app.use(express.static(path.join(__dirname, '../public')));
 http.createServer(app).listen(3000);
