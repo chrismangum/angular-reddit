34f87e0df64c6289c7d53fce88c0a862c9ba2505
Chris Mangum <mtdbass5@gmail.com>
2014-01-28 21:27:07 -0500

    added new theme, refactored code that sets theme and that adds library services, altered some indentation

--- public/js/app.js
+++ public/js/app.js
@@ -1,65 +1,65 @@
 var app = angular.module('app', ['ngRoute']);
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-  $routeProvider.when('/r/:subreddit', {
-    templateUrl: '/static/template.html',
-    controller: 'tmpCtrl'
-  });
-  $routeProvider.when('/r/:subreddit/comments/:thread/:title', {
-    templateUrl: '/static/template.html',
-    controller: 'tmpCtrl'
-  });
-  $locationProvider.html5Mode(true);
-}]);
-
-app.controller('mainCtrl', ['$scope', 'underscore', 'jQuery', 'localStorage', '$sce', function ($scope, _, $, storage, $sce) {
-  var $theme = $('.theme');
-  var $body = $(document.body);
-  var dummyEl = document.createElement('div');
-
-  $scope.parseHtml = function (body_html) {
-    return $sce.trustAsHtml(_.unescape(body_html));
-  };
-  $scope.themes = {
-    'Amelia': '/static/css/bootstrap-themes/amelia.min.css',
-    'Cyborg': '/static/css/bootstrap-themes/cyborg.min.css',
-    'Default': '/static/css/bootstrap-themes/default.min.css',
-    'Slate': '/static/css/bootstrap-themes/slate.min.css',
-    'Yeti': '/static/css/bootstrap-themes/yeti.min.css'
-  };
-  $scope.subreddits = ['r/commandline', 'r/linux', 'r/programming'];
-  $scope.setTheme = function(name) {
-    storage.theme = name;
-    $theme.attr('href', $scope.themes[name]);
-    $body.attr('id', name);
-  };
-  if (!storage.theme) {
-    storage.theme = 'Cyborg';
+app.config(['$routeProvider', '$locationProvider',
+  function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/r/:subreddit', {
+      templateUrl: '/static/template.html',
+      controller: 'tmpCtrl'
+    });
+    $routeProvider.when('/r/:subreddit/comments/:thread/:title', {
+      templateUrl: '/static/template.html',
+      controller: 'tmpCtrl'
+    });
+    $locationProvider.html5Mode(true);
   }
-  $scope.setTheme(storage.theme);
+]);
 
-  $(window).on('scroll', function () {
-    if ($(document).height() === $(this).scrollTop() + this.innerHeight) {
-      console.log('bottom');
+app.controller('mainCtrl', ['$scope', '_', 'jQuery', 'localStorage', '$sce',
+  function ($scope, _, $, storage, $sce) {
+    var $theme = $('.theme');
+    var $body = $(document.body);
+    var dummyEl = document.createElement('div');
+
+    $scope.parseHtml = function (body_html) {
+      return $sce.trustAsHtml(_.unescape(body_html));
+    };
+    $scope.themes = ['Amelia', 'Cyborg', 'Default', 'Slate', 'Yeti'];
+    $scope.subreddits = ['r/commandline', 'r/linux', 'r/programming'];
+    $scope.setTheme = function(name) {
+      storage.theme = name;
+      $theme.attr('href', '/static/css/bootstrap-themes/' + name.toLowerCase() + '.min.css');
+      $body.attr('id', name);
+    };
+    if (!storage.theme) {
+      storage.theme = 'Cyborg';
     }
-  });
-}]);
+    $scope.setTheme(storage.theme);
 
-app.controller('tmpCtrl', ['$scope', 'http', '$routeParams', 'parse', function ($scope, http, $routeParams, parse) {
-  $scope.posts = [];
-  $scope.comments = [];
+    $(window).on('scroll', function () {
+      if ($(document).height() === $(this).scrollTop() + this.innerHeight) {
+        console.log('bottom');
+      }
+    });
+  }
+]);
 
-  http.get(function (data) {
-    if ($routeParams.thread) {
-      $scope.posts = parse.posts(data[0]);
-      $scope.comments = parse.comments(data[1]);
-    } else {
-      $scope.posts = parse.posts(data);
-    }
-    document.title = $routeParams.subreddit;
-    $scope.$apply();
-  });
-}]);
+app.controller('tmpCtrl', ['$scope', 'http', '$routeParams', 'parse',
+  function ($scope, http, $routeParams, parse) {
+    $scope.posts = [];
+    $scope.comments = [];
+
+    http.get(function (data) {
+      if ($routeParams.thread) {
+        $scope.posts = parse.posts(data[0]);
+        $scope.comments = parse.comments(data[1]);
+      } else {
+        $scope.posts = parse.posts(data);
+      }
+      document.title = $routeParams.subreddit;
+      $scope.$apply();
+    });
+  }
+]);
 
 app.factory('parse', function () {
   var parse = {};
@@ -93,39 +93,31 @@ app.filter('timeago', ['moment', function (moment) {
   };
 }]);
 
-app.factory('http', ['$httpBackend', '$routeParams', 'jQuery', 'underscore', function ($httpBackend, $routeParams, $, _) {
-  function buildUrl() {
-    var url = 'http://www.reddit.com/r/',
-      params = _.extend({}, $routeParams);
-    url += params.subreddit;
-    url += params.thread ? '/comments/' + params.thread : '';
-    url += params.sort ? '/' + params.sort : '';
-    params = _.omit(params, 'subreddit', 'thread');
-    params.limit = 1000;
-    url += '.json?';
-    return url + $.param(params);
-  }
-  return {
-    get: function (callback) {
-      $httpBackend('GET', buildUrl(), null, function (status, data) {
-        callback(angular.fromJson(data));
-      });
+app.factory('http', ['$httpBackend', '$routeParams', 'jQuery', '_',
+  function ($httpBackend, $routeParams, $, _) {
+    function buildUrl() {
+      var url = 'http://www.reddit.com/r/',
+        params = _.extend({}, $routeParams);
+      url += params.subreddit;
+      url += params.thread ? '/comments/' + params.thread : '';
+      url += params.sort ? '/' + params.sort : '';
+      params = _.omit(params, 'subreddit', 'thread');
+      params.limit = 1000;
+      url += '.json?';
+      return url + $.param(params);
     }
-  };
-}]);
+    return {
+      get: function (callback) {
+        $httpBackend('GET', buildUrl(), null, function (status, data) {
+          callback(angular.fromJson(data));
+        });
+      }
+    };
+  }
+]);
 
-app.factory('moment', function () {
-  return moment;
-});
-
-app.factory('underscore', function () {
-  return _;
-});
-
-app.factory('jQuery', function () {
-  return jQuery;
-});
-
-app.factory('localStorage', function () {
-  return localStorage;
+_.each(['moment', '_', 'jQuery', 'localStorage'], function (item) {
+  app.factory(item, function () {
+    return window[item];
+  });
 });
