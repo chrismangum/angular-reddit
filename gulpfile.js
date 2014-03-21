de63d557e63d8ac0b8b88003d085634fc6f47604
Chris Mangum <mtdbass5@gmail.com>
2014-03-20 20:53:56 -0400

    added gulp-stylus

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify'),
   concat = require('gulp-concat'),
   jade = require('gulp-jade'),
-  nodemon = require('gulp-nodemon');
+  nodemon = require('gulp-nodemon'),
+  stylus = require('gulp-stylus');
 
 var paths = {
   js: 'public/js/*.coffee',
-  jade: 'views/index.jade'
+  jade: 'views/*.jade',
+  stylus: 'public/css/*.styl',
 };
 
 gulp.task('scripts', function () {
-  return gulp.src(paths.js)
+  gulp.src(paths.js)
     .pipe(coffee())
     .pipe(uglify())
     .pipe(concat('app.min.js'))
@@ -19,22 +21,32 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('jade', function () {
-  return gulp.src(paths.jade)
+  gulp.src(paths.jade)
     .pipe(jade())
     .pipe(gulp.dest('public/'));
+});
+
+gulp.task('stylus', function () {
+  gulp.src(paths.stylus)
+    .pipe(stylus({
+      set: ['compress'],
+      use: ['nib']
+    }))
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('nodemon', function () {
   nodemon({
     script: 'server/app.js',
     ext: 'js,coffee',
-    ignore: ['public/**']
+    ignore: ['public/**', 'node_modules/**']
   });
 });
 
 gulp.task('watch', function () {
   gulp.watch(paths.js, ['scripts']);
   gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.stylus, ['stylus']);
 });
 
-gulp.task('default', ['scripts', 'jade', 'watch', 'nodemon']);
+gulp.task('default', ['scripts', 'jade', 'stylus', 'watch', 'nodemon']);
