a94c8d7d4e115c9bbe56f95eb80b9f2de76a7d56
Chris Mangum <mtdbass5@gmail.com>
2014-06-10 17:23:46 -0400

    heroku changes

--- server/app.js
+++ server/app.js
@@ -9,4 +9,4 @@ app.get('*', function (req, res) {
   res.sendfile(path.join(__dirname, "../public/index.html"));
 });
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(process.env.PORT || 3000);
